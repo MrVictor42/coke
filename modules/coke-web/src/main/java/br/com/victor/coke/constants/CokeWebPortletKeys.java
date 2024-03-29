915f14e1d8cd0034b5823c5442dbdc0bc8e43119
Victor Hugo Lopes Mota <victor.mota@seatecnologia.com.br>
2024-03-28 21:50:20 -0300

    Starting project

---
+++ modules/coke-web/src/main/java/br/com/victor/coke/constants/CokeWebPortletKeys.java
@@ -0,0 +1,11 @@
+package br.com.victor.coke.constants;
+
+/**
+ * @author victor
+ */
+public class CokeWebPortletKeys {
+
+	public static final String COKEWEB =
+		"br_com_victor_coke_CokeWebPortlet";
+
+}
---
+++ modules/coke-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="cokeweb.caption"/></b>
+</p>
